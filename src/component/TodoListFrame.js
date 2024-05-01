34203d7a404cf70d1dbcc615c85242f3df39817f
HYH <dudgns3864@gmail.com>
2024-05-01 23:02:54 +0900

    todoList_inputCheck_func

--- src/component/TodoListFrame.js
+++ src/component/TodoListFrame.js
@@ -1,8 +1,8 @@
-import { React, useCallback } from 'react';
+import { React, useCallback, useRef } from 'react';
 import todoListFrame from"./TodoListFrame.module.css";
 
 function TodoListFrame({todoList, setTodoList}) {
-  let onDelete = useCallback((id)=>{
+  const onDelete = useCallback((id)=>{
       setTodoList((prev)=>prev.filter((_,listId)=>listId !== id))  
   },[]);
 
@@ -11,6 +11,7 @@ function TodoListFrame({todoList, setTodoList}) {
       {todoList.map((content,id)=>{
         return(
           <div key={id} className={todoListFrame.listItem}>
+            <input type='checkbox' className={todoListFrame.checkBox}/>
             {content}
             <button className={todoListFrame.delBtn} onClick={()=> onDelete(id)}>❌</button>
           </div>
